d24a41d74c3fb8f238cdbff1b432a27dd330860d
Kiraandaja <kiraandaja@gmail.com>
2022-06-02 13:50:30 -0700

    second commit

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        
+            public bool IsAPalindrome(string word)
+            {
+                //reverse the word
+                var reversed = "";
+                for (int i = word.Length - 1; i>=0; i--)
+                {
+                    reversed += word[i];
+                }
+                //compare the original w/ the reversed
+                if (reversed.ToLower() == word.ToLower())
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+               
+
+               
+
+       
+    }
+}
